7dadb14692357051da5bb0b769212a66bb7965bf
8Doker8 <dmitrijs.bezrebrijs@gmail.com>
2023-12-16 13:15:22 +0200

    Sdelal

--- main.py
+++ main.py
@@ -14,3 +14,5 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Создать программу которая берет любой текстовый инпут и переделывает текст в код Морза
